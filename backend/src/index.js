69d6c58d71f6db7d077e3211662cfcfd772e138e
Bobybuu <c43057772@gmail.com>
2025-01-18 14:42:38 +0300

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 import cors  from "cors";
 
 dotenv.config();
-const app = express();
+import path from "path";
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +24,16 @@ app.use(cors(
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
+server.listen(PORT, () => {
   console.log("Server running on PORT:"+PORT);
   connectDB()
 });
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -26,8 +26,8 @@ const ProfilePage = () => {
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
-            <h1 className="text-2xl font-semibold ">Artist Profile</h1>
-            <p className="mt-2">Artist profile information</p>
+            <h1 className="text-2xl font-semibold ">Profile</h1>
+            <p className="mt-2">Profile information</p>
           </div>
 
           {/* avatar upload section */}
